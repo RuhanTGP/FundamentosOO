093a2941e316552bbedfd8ab598de76ca6af652f
Vinicius Camargo Andrade <vcandrade@utfpr.edu.br>
2023-03-17 20:49:27 -0300

    added class 04

--- src/aula04/metodos/Carro.java
+++ src/aula04/metodos/Carro.java
@@ -8,27 +8,74 @@ public class Carro {
 	int ano;
 	String placa;
 	String cor;
+	boolean ligado = false;
+	int velocidade = 0;
 
 	// métodos da classe
 	public void ligar() {
-		System.out.println(marca + " " + modelo + " ligou.");
+
+		if (!ligado) {
+			
+			ligado = true;
+			System.out.println(marca + " " + modelo + " ligou.");
+		
+		} else {
+			
+			System.out.println(marca + " " + modelo + " já encontra-se ligado");
+		}
 	}
 
 	public void desligar() {
-		System.out.println(marca + " " + modelo + " desligou.");
+
+		if (ligado && velocidade == 0) {
+		
+			ligado = false;
+			System.out.println(marca + " " + modelo + " desligou.");
+		
+		} else {
+		
+			System.out.println("Não foi possível desligar o " + marca + " " + modelo);
+		}
 	}
 
 	public void acelerar() {
-		System.out.println(marca + " " + modelo + " acelerou.");
+
+		if (ligado) {
+
+			velocidade += 10;
+
+			if (velocidade > 200) {
+
+				System.out.println(marca + " " + modelo + " atingiu a velocidade máxima.");
+				velocidade = 200;
+			}
+
+		} else {
+			System.out.println(marca + " " + modelo + " não encontra-se ligado.");
+		}
 	}
 
 	public void frear() {
-		System.out.println(marca + " " + modelo + " freou.");
+
+		if (ligado) {
+
+			velocidade -= 10;
+
+			if (velocidade < 0) {
+
+				velocidade = 0;
+				System.out.println(marca + " " + modelo + " encontra-se parado.");
+			}
+
+		} else {
+
+			System.out.println(marca + " " + modelo + " não encontra-se ligado.");
+		}
 	}
 
 	@Override
 	public String toString() {
 		return "Carro [modelo=" + modelo + ", marca=" + marca + ", ano=" + ano + ", placa=" + placa + ", cor=" + cor
-				+ "]";
+				+ ", ligado=" + ligado + ", velocidade=" + velocidade + "]";
 	}
 }
--- src/aula04/metodos/CarroTeste.java
+++ src/aula04/metodos/CarroTeste.java
@@ -1,17 +1,11 @@
 package aula04.metodos;
 
-import java.util.Scanner;
-
 public class CarroTeste {
 
 	public static void main(String[] args) {
 
-		Scanner input = new Scanner(System.in);
-
 		// instanciação de objetos
 		Carro carro1 = new Carro();
-		Carro carro2 = new Carro();
-		Carro carro3 = new Carro();
 
 		// atribuir valores aos atributos de cada carro
 		carro1.modelo = "Gol";
@@ -20,27 +14,16 @@ public class CarroTeste {
 		carro1.placa = "ABC-1234";
 		carro1.cor = "Amarelo";
 
-		carro2.modelo = "Onix";
-		carro2.marca = "Chevrolet";
-		carro2.ano = 2015;
-		carro2.placa = "MNO-9876";
-		carro2.cor = "Preto";
-
-		carro3.modelo = "Ka";
-		carro3.marca = "Ford";
-		carro3.ano = 2010;
-		carro3.placa = "XYZ-1596";
-		carro3.cor = "Branco";
-
-		// obtenção dos valores dos atributos de cada carro
-		// e impressão via console
+		// testes dos métodos do carro
 		System.out.println(carro1);
-		System.out.println(carro2);
-		System.out.println(carro3);
 
-		carro3.ligar();
-		carro3.ligar();
-		carro3.acelerar();
 		carro1.ligar();
+		carro1.acelerar();
+		System.out.println(carro1);
+
+		carro1.frear();
+		carro1.frear();
+		carro1.frear();
+		System.out.println(carro1);
 	}
 }
