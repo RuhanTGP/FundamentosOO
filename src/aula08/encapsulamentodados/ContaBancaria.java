c4d47e6d2dd936af1d84359c2880cbfc859d9e2d
Vinicius Camargo Andrade <vcandrade@utfpr.edu.br>
2023-03-31 20:14:13 -0300

    added class 08

---
+++ src/aula08/encapsulamentodados/ContaBancaria.java
@@ -0,0 +1,73 @@
+package aula08.encapsulamentodados;
+
+public class ContaBancaria {
+
+	private int numero;
+	private String nomeTitular;
+	private double saldo;
+	
+	public ContaBancaria(int numero, String nomeTitular) {
+		
+		this.numero = numero;
+		this.nomeTitular = nomeTitular;
+		this.saldo = 0.00;
+	}
+	
+	public void sacar(double valor) {
+		
+		if(valor <= this.saldo) {
+			
+			this.saldo -= valor;
+			System.out.println("Saque de R$" + valor + " realizado com sucesso.");
+			
+		} else {
+			
+			System.out.println("Saldo insuficiente.");
+		}
+	}
+	
+	public void depositar(double valor) {
+		
+		this.saldo += valor;
+		System.out.println("Depósito de R$" + valor + " realizado com sucesso.");
+		
+		this.calcularRendimentos();
+	}
+	
+	public void imprimirSaldo() {
+		
+		System.out.println("======== SALDO ========");
+		System.out.println("Número da Conta: " + this.numero);
+		System.out.println("Nome do titular: " + this.nomeTitular);
+		System.out.println("Saldo: R$" + String.format("%.2f", this.saldo));
+		System.out.println("=======================");
+	}
+	
+	private void calcularRendimentos() {
+		
+		this.saldo *= 1.10;
+	}
+
+	public String getNomeTitular() {
+		return nomeTitular;
+	}
+
+	public void setNomeTitular(String nomeTitular) {
+		this.nomeTitular = nomeTitular;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/aula08/encapsulamentodados/ContaBancariaTeste.java
@@ -0,0 +1,20 @@
+package aula08.encapsulamentodados;
+
+public class ContaBancariaTeste {
+
+	public static void main(String[] args) {
+		
+		ContaBancaria conta1 = new ContaBancaria(111, "João da Silva");
+		conta1.imprimirSaldo();
+		
+		conta1.setNomeTitular("Maria de Oliveira");
+		
+		System.out.println(conta1.getNumero());
+		System.out.println(conta1.getNomeTitular());
+	}
+}
+
+
+
+
+
